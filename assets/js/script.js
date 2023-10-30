4a1ca3aafad26a8e7f43830cf2172bd941252fdf
Corentin Gerard <corentin_gerard@mbp-de-corentin.lan>
2023-10-30 15:01:47 +0100

    connection to Northflank

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("document loaded");
+const $ = document;
 
-  document.querySelector("form").addEventListener("submit", async (event) => {
-    console.log("submit");
+$.addEventListener("DOMContentLoaded", () => {
+  const form = $.querySelector("#contact-form");
+  const submitButton = $.querySelector("#submit-btn");
+
+  // Function to empty the form
+  const cleanForm = () => {
+    form.reset();
+  };
+
+  // Function to deactivate the submit button
+  const isDisabled = () => {
+    submitButton.setAttribute("disabled", "disabled");
+    submitButton.classList.add("disabled-btn");
+  };
+
+  // Function to reactivate the submit button
+  const isEnabled = () => {
+    submitButton.removeAttribute("disabled");
+    submitButton.classList.remove("disabled-btn");
+  };
+
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
+    isDisabled();
 
     const data = {
-      firstname: document.querySelector("#firstname").value,
-      lastname: document.querySelector("#lastname").value,
-      email: document.querySelector("#email").value,
-      message: document.querySelector("#message").value,
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      subject: $.querySelector("#subject").value,
+      message: $.querySelector("#message").value,
     };
 
-    /* console.log(data); */
+    try {
+      const response = await axios.post(
+        "https://site--form-backend--c9n4rr6v2pd4.code.run/",
+        data
+      );
 
-    const response = await axios.post("http://localhost:3000/form", data);
-    console.log(response);
+      if (response.status === 200) {
+        alert("Votre formulaire a bien été envoyé");
+        cleanForm();
+        isEnabled();
+      }
+    } catch (error) {
+      if (error.response?.data.message === "Missing parameters") {
+        alert("Veuillez remplir tous les champs du formulaire");
+      } else {
+        alert("Une erreur est survenue");
+        cleanForm();
+      }
+
+      isEnabled();
+    }
   });
 });
